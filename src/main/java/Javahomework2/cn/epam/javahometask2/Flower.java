e96fce574d2e387f7ccb776afd2514b3bed66b0f
janeyzhou1 <janey_zhou@epam.com>
2016-03-02 17:36:05 +0800

    update project

--- src/main/java/Javahomework2/cn/epam/javahometask2/Flower.java
+++ src/main/java/Javahomework2/cn/epam/javahometask2/Flower.java
@@ -1,5 +1,63 @@
 package Javahomework2.cn.epam.javahometask2;
 
-public class Flower {
+import Javahomework2.cn.epam.javahometask2.Bouquet;;
+
+
+public class Flower extends Bouquet{
+	
+	Bouquet flowerbasket;
+
+	int price;
+	int quantity;
+	
+	
+	public Flower(String name) {
+		super(name);
+	}
+
+	public Flower(String name, int price, int quantity, Bouquet flowerbasket){
+		super(name);
+		this.price = price;
+		this.quantity = quantity;
+		this.flowerbasket = flowerbasket;
+			
+	}
+	
+	public Bouquet getflowerbasket() {
+		return flowerbasket;
+	}
+
+	public void setflowerbasket(Bouquet flowerbasket) {
+		this.flowerbasket = flowerbasket;
+	}
+		
+	public int getPrice(){
+		return price;
+	}
+	
+	public void setPrice(int price){
+		this.price = price;
+	}
+	
+	public int getQuantity(){
+		return quantity;
+	}
+	
+	public void setQuantity(int quantity){
+		this.quantity = quantity;
+	}
+	
+	
+	public int addFlowersToBouquet() {
+		return this.price*this.quantity;
+	}
+	
+	public String getinfo(){
+		return this.quantity + " " + this.name +" with Unit Pirce: " + this.price;
+	}
+	
+	
 
 }
+
+
--- src/main/java/Javahomework2/cn/epam/javahometask2/Runner.java
+++ src/main/java/Javahomework2/cn/epam/javahometask2/Runner.java
@@ -1,5 +1,89 @@
 package Javahomework2.cn.epam.javahometask2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import Javahomework2.cn.epam.javahometask2.Bouquet;
+import Javahomework2.cn.epam.javahometask2.Flower;
+
+
+
 public class Runner {
 
+	public static void main(String[] args) {
+		boolean repeat = true;
+		
+		Bouquet flowerbasket = new Bouquet ("Businessflowerbasket");
+
+						
+		List<Flower> list = new ArrayList<Flower>();
+		
+		while(repeat) {
+			System.out.println("1. =add flowers to flowerbasket");
+			System.out.println("2. =find flower");
+			System.out.println("3. =show list");
+			System.out.println("4. =Show bouquet information");
+			System.out.println("0. =exit");
+			
+			Scanner scanner = new Scanner (System.in);
+			int action = scanner.nextInt();
+			
+			switch(action){
+			case 0: 
+				repeat = false;
+				System.out.println("Completed!");
+				break;
+				
+			case 1:
+				System.out.println("Type the flower name:");
+				String flowername = new Scanner(System.in).nextLine();
+				System.out.println("Type the flower price:");
+				int flowerprice = Integer.parseInt(new Scanner(System.in).nextLine());
+				System.out.println("Type the flower quantity:");
+				int flowerquantity = Integer.parseInt(new Scanner(System.in).nextLine());
+										
+				list.add(new Flower(flowername, flowerprice, flowerquantity,flowerbasket));
+				
+				break;
+				
+			case 2:
+				System.out.println("Type a name to search");
+				String name = new Scanner(System.in).nextLine();
+				Utils.findFlowers(name, list);
+				break;
+				
+				
+										
+			case 3:
+				for(Flower flower : list){
+					System.out.println(flower.getinfo());
+					
+				}
+							
+				break;
+				
+			case 4:
+				for(Flower flower : list){
+					flowerbasket.totalprice += flower.addFlowersToBouquet();
+					System.out.println(flower.getinfo() + " added to " + flowerbasket.name);
+
+				}
+				
+				System.out.println(flowerbasket.name + " total price is " +flowerbasket.totalprice);
+
+				
+				break;
+				
+							
+				default: 
+					System.out.println("Incorrect value! Please, try again!");
+					break;
+			}
+			
+		}
+
+	}
+
 }
+
